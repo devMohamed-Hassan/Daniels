2772a6848a642d25d2582f0fa34eef3ef44cea86
Mohamed Hassan <mohamed.h.ismael@gmail.com>
2024-11-28 02:02:48 +0200

    Add HTML, CSS, JS, images and Bootstrap files.

---
+++ js/script.js
@@ -0,0 +1,45 @@
+//console.log("script loaded!");
+
+<!-- navbar -->
+
+window.addEventListener("scroll", function () {
+    var x = document.getElementById("navbar-id");
+    var y = document.getElementById("hero").offsetHeight;
+    if (window.scrollY > y) {
+        x.classList.add("navbar-solid");
+    } else {
+        x.classList.remove("navbar-solid");
+    }
+});
+
+<!-- counter -->
+
+function counter() {
+    var cnt = document.querySelectorAll('[data-count]');
+    cnt.forEach(counter => {
+        var x = +counter.getAttribute('data-count');
+        var y = x / 100;
+
+        function up() {
+            var cur = +counter.innerText;
+            if (cur < x) {
+                counter.innerText = Math.ceil(cur + y);
+                setTimeout(up, 20);
+            } else {
+                counter.innerText = x;
+            }
+        }
+
+        up(); // recall
+    });
+}
+
+document.addEventListener("DOMContentLoaded", counter);
+
+<!-- hero -->
+
+var typed = new Typed("#element", {
+    strings: ["LARRY Daniels", "Developer", "Designer"],
+    typeSpeed: 50,
+    loop: true,
+});
